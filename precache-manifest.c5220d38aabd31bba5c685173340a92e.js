ccb429d1a1c3256b42e07363ffe132635061c109
pixel <taraspixel@gmail.com>
2019-06-08 22:37:37 +0300

    Updates

---
+++ precache-manifest.c5220d38aabd31bba5c685173340a92e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cards/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fcd8d05ca2a2981b7680f6b1b322c80e",
+    "url": "/cards/static/media/loading.fcd8d05c.gif"
+  },
+  {
+    "revision": "ee7cf3a52179a8c6c89f",
+    "url": "/cards/static/js/runtime~main.36607aec.js"
+  },
+  {
+    "revision": "16002c4874d407811c91",
+    "url": "/cards/static/js/main.460f270b.chunk.js"
+  },
+  {
+    "revision": "736c35ab9103b538e5f2",
+    "url": "/cards/static/js/2.5ca72720.chunk.js"
+  },
+  {
+    "revision": "16002c4874d407811c91",
+    "url": "/cards/static/css/main.ad43d122.chunk.css"
+  },
+  {
+    "revision": "f5c1ee5894ed94489bdf082a644cb2a0",
+    "url": "/cards/index.html"
+  }
+];
